5240e97234335ff99723dc37f25cb6006b46d8f5
Ji Won Park <parj2@gene.com>
2023-10-23 22:42:32 -0700

    Put tensors on same device

--- mm/_utils.py
+++ mm/_utils.py
@@ -2,20 +2,20 @@ import torch
 
 
 def marginalize_1d(orig_tensor, keep_idx):
-    keep_idx = torch.tensor(keep_idx)
+    keep_idx = torch.tensor(keep_idx).to(orig_tensor.device)
     marginalized = torch.index_select(
-        orig_tensor, 
+        orig_tensor,
         dim=-1, index=keep_idx)
     return marginalized
 
 
 def marginalize_2d(orig_tensor, keep_idx, keep_idx_last_dim: list = None):
-    keep_idx = torch.tensor(keep_idx)
+    keep_idx = torch.tensor(keep_idx).to(orig_tensor.device)
     if keep_idx_last_dim is None:
         keep_idx_last_dim = keep_idx
     else:
         keep_idx_last_dim = torch.tensor(keep_idx_last_dim)
     marginalized = torch.index_select(
-        torch.index_select(orig_tensor, dim=-2, index=keep_idx), 
+        torch.index_select(orig_tensor, dim=-2, index=keep_idx),
         dim=-1, index=keep_idx_last_dim)
     return marginalized
