45a53aba91346d07d156e8d8be2ee29e338ddc41
Ji Won Park <jiwoncpark@gmail.com>
2023-10-23 20:37:56 -0700

    Package up

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+      name='gmm',
+      version='v0.10',
+      author='Ji Won Park',
+      packages=find_packages(),
+      license='LICENSE.md',
+      description='Methods for inference of external convergence',
+      long_description=open("README.md").read(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/jiwoncpark/gaussian-mixture-models',
+      include_package_data=True,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      keywords='statistics'
+      )
